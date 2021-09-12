81172f811670d29fc742073e6566362d018abc97
Rawnak Chourasia <rnkchr0@gmail.com>
2021-09-13 00:12:50 +0530

    ngStyle - also ternary if else usage

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,20 @@ import { Component } from "@angular/core";
 })
 
 export class ServerComponent{
+
+    serverStatus='offline';
+    serverId=10;
+
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline' ;
+    }
+
+    showServerName(){
+        return this.serverStatus;
+    }
+
+    getColor(){
+        return this.serverStatus==='online' ? 'lightgreen' : 'skyblue';
+    }
     
 }
